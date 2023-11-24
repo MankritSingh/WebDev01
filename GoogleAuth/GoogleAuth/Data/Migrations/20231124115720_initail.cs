8d0dd7ae5e73fa6e8cf82f0bf6a0b07e92175d9c
MankritSingh <singhmankrit11@gmail.com>
2023-11-24 18:53:00 +0530

    Project Upload

---
+++ GoogleAuth/GoogleAuth/Data/Migrations/20231124115720_initail.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GoogleAuth.Data.Migrations
+{
+    public partial class initail : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
